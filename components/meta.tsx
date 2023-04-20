a4d9ed080fe53e254a28642be9b8b6ac733b9268
luisra99 <luisraul.alfonsoc@gmail.com>
2023-04-20 03:41:34 -0400

    as

--- components/meta.tsx
+++ components/meta.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 const Meta = () => (
 	<Head>
-		<title>Rice Bowl</title>
+		<title>Bodega Virtual</title>
 		<meta charSet='utf-8' />
 		<meta name='mobile-web-app-capable' content='yes' />
 		<meta name='apple-mobile-web-app-capable' content='yes' />
@@ -10,9 +10,9 @@ const Meta = () => (
 			name='apple-mobile-web-app-status-bar-style'
 			content='black-translucent'
 		/>
-		<meta name='apple-mobile-web-app-title' content='Rice Bowl' />
-		<meta name='application-name' content='Rice Bowl' />
-		<meta name='description' content='Bring your own ingredients' />
+		<meta name='apple-mobile-web-app-title' content='Bodega Virtual' />
+		<meta name='application-name' content='Bodega Virtual' />
+		<meta name='description' content='' />
 		<meta
 			name='theme-color'
 			content='#f4f4f5'
@@ -27,7 +27,7 @@ const Meta = () => (
 			name='viewport'
 			content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
 		/>
-		<link rel='apple-touch-icon' href='/images/icon-maskable-512.png' />
+		<link rel='apple-touch-icon' href='/images/maskable_icon_x512.png' />
 		<link rel='icon' type='image/png' href='/images/favicon.png' />
 		<link rel='manifest' href='/manifest.json' />
 	</Head>
