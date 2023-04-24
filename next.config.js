473f8294667dba84e02cc4a2db07de671d1d1fef
luisra99 <luisraul.alfonsoc@gmail.com>
2023-04-24 03:42:25 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
-const withPWA = require('next-pwa')({
-	disable: process.env.NODE_ENV === 'development',
-	dest: 'public',
-	register: true,
-	skipWaiting: false,
-	runtimeCaching,
-})
-const nextConfig = withPWA({})
 
-module.exports = nextConfig
-module.exports = {
+module.exports = withPWA({
+	pwa: {
+		disable: process.env.NODE_ENV === 'development',
+		dest: 'public',
+		register: true,
+		skipWaiting: false,
+		runtimeCaching,
+	},
 	reactStrictMode: true,
 	env: {
 		WSO2: {
@@ -41,4 +39,4 @@ module.exports = {
 			},
 		},
 	},
-}
+})
