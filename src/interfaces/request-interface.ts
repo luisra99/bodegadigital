33fbc338acd2163678a6b3071f43825cd96c01de
luisra99 <luisraul.alfonsoc@gmail.com>
2023-04-25 14:12:15 -0400

    new

---
+++ src/interfaces/request-interface.ts
@@ -0,0 +1,32 @@
+export interface RequestData {
+  method: string;
+  params: object;
+  url: string;
+  data: object;
+}
+
+export interface RequestOptions {
+  method: string;
+  url: string;
+  data: RequestData;
+}
+export interface GetDataParams {
+  key: string;
+  api: string;
+  action: string;
+  method?: "GET" | "POST" | "get" | "post";
+  params?: object;
+  data?: object;
+}
+export interface ApiConfig {
+  AM_PROTOCOL: string;
+  AM_IP: string;
+  AM_PORT: string;
+  AM_CONSUMER_KEY: string;
+  AM_CONSUMER_SECRET: string;
+  AM_TOKEN_ENDPOINT: string;
+  GW_PROTOCOL: string;
+  GW_IP: string;
+  GW_PORT: string;
+  API: string;
+}
