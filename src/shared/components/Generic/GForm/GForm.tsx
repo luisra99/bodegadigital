b2ed9bfc862e6f9ad05f4a95a49fbd2211ec0329
luisra99 <luisraul.alfonsoc@gmail.com>
2023-05-07 18:22:13 -0400

    deps

--- src/shared/components/Generic/GForm/GForm.tsx
+++ src/shared/components/Generic/GForm/GForm.tsx
@@ -1,44 +1,4 @@
-import { useEffect, useState } from 'react';
-
-import SaveAsIcon from '@mui/icons-material/SaveAs';
-import SaveRoundedIcon from '@mui/icons-material/SaveRounded';
-import {
-  Button,
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  FormHelperText,
-  FormLabel,
-  Input,
-  InputAdornment,
-  InputLabel,
-  MenuItem,
-  Radio,
-  RadioGroup,
-  Select,
-  TextField,
-} from '@mui/material';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-
-import { ErrorMessage, Field, Form, Formik, useFormikContext, FormikProps } from 'formik';
-import * as Yup from 'yup';
-
-import { ControlType } from '@/enums/form-enums';
-import {
-  DictionaryValidationBasic,
-  DictionaryValidationFunctions,
-  GControlProps,
-  GFormControl,
-  MyObject,
-  Option,
-  IFormikProps,
-  ValidatorsSchema,
-  GFormButtons,
-  GFormProps,
-} from '@/shared/interfaces/form-control';
-
-import axios from 'axios';
+import { GFormProps } from '@/shared/interfaces/form-control';
 
 export default function GForm(props: GFormProps) {
   const {
