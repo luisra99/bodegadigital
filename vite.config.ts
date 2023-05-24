5b0dcd759ac09a92f12947a6d5e46cb33a60c3bb
luisra99 <luisraul.alfonsoc@gmail.com>
2023-05-24 15:53:59 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       // srcDir:'src/services',
       // filename:'sw.js',
       manifest,
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'sw.js'],
       // switch to "true" to enable sw on development
       devOptions: {
         enabled: false,
