97a49b572f857c234dc94d32d53c96de8d223125
luisra99 <luisraul.alfonsoc@gmail.com>
2023-05-26 11:28:03 +0100

    j

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate" ,
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'sw.ts',
+    
       manifest,
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       // switch to "true" to enable sw on development
