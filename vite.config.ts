04402486fd84f4279143e5b3b86eef6ef50702e8
luisra99 <luisraul.alfonsoc@gmail.com>
2023-05-30 00:34:49 +0100

    nexts steps

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,47 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-
+import { rollup, InputOptions, OutputOptions } from 'rollup'
+import rollupPluginTypescript from 'rollup-plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
+
+const CompileTsServiceWorker = () => ({
+  name: 'compile-typescript-service-worker',
+  async writeBundle(_options, _outputBundle) {
+    const inputOptions: InputOptions = {
+      input: 'src/shared/sw-custom.ts',
+      plugins: [rollupPluginTypescript(), nodeResolve()],
+    }
+    const outputOptions: OutputOptions = {
+      file: 'dist/sw-custom.js',
+      format: 'es',
+    }
+    const bundle = await rollup(inputOptions)
+    await bundle.write(outputOptions)
+    await bundle.close()
+  }
+})
+
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate" ,
-    
       manifest,
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       // switch to "true" to enable sw on development
       devOptions: {
-        enabled: false,
+        enabled: true,
       },
       workbox: {
         // importScripts:['sw.js'],
         globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'],
       },
     }),
+    CompileTsServiceWorker()
   ],
   resolve: {
     alias: {
@@ -31,3 +51,5 @@ export default defineConfig({
   },
   envPrefix: 'VITE_',
 });
+
+
