81945e3d91a3a3c51233e597f711da4172f7338a
luisra99 <luisraul.alfonsoc@gmail.com>
2023-05-30 00:57:50 +0100

    undo3

--- src/shared/sw-custom.ts
+++ src/shared/sw-custom.ts
@@ -1,8 +1,8 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="esnext" />
-/// <reference lib="webworker" />
-const sw = self as unknown as ServiceWorkerGlobalScope & typeof globalThis;
+// /// <reference no-default-lib="true"/>
+// /// <reference lib="esnext" />
+// /// <reference lib="webworker" />
+// const sw = self as unknown as ServiceWorkerGlobalScope & typeof globalThis;
 
-sw.addEventListener('install', (event) => {
-  // ...
-})
+// sw.addEventListener('install', (event) => {
+//   // ...
+// })
