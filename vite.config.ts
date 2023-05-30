c6257487906f9fb6313acd808534195086b140f0
luisra99 <luisraul.alfonsoc@gmail.com>
2023-05-30 16:19:15 +0100

    l

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ const pwaOptions: Partial<VitePWAOptions> = {
   },
   filename: 'custom-service-worker.ts',
 };
+pwaOptions.srcDir = 'src';
 // https://vitejs.dev/config/
 
 export default defineConfig({
