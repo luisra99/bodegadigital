68e1671cfd450c3f60369d69fc9bcec0343100be
luisra99 <luisraul.alfonsoc@gmail.com>
2023-05-31 10:36:37 +0100

    sw

--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,16 @@
 const CACHE_NAME = 'my-cache-v2';
-
+self.__WB_MANIFEST;
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     (async function () {
       console.log('La url es ', event.request.url);
       const cachedResponse = await caches.match(event.request);
-      if (cachedResponse) return cachedResponse;
-
+      if (cachedResponse) {
+        let url= event.request.url.
+        if(url.contain)
+        console.table(cachedResponse)
+        return cachedResponse;
+      }
       try {
         const fetchResponse = await fetch(event.request);
         const cache = await caches.open(CACHE_NAME);
