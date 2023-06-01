a20c9914c051cc9a0774f4c95801e288008eb04b
luisra99 <luisraul.alfonsoc@gmail.com>
2023-06-01 10:40:56 +0100

    sw

--- src/main.tsx
+++ src/main.tsx
@@ -12,16 +12,6 @@ import welcome from '@/utils/welcome';
 //   navigator.serviceWorker.register('./service/sw.js')
 // })
 Promise.all([import('@/Root'), import('@/App')]).then(([{ default: render }, { default: App }]) => {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
-      try {
-        const registration = await navigator.serviceWorker.register('./sw.js');
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      } catch (err) {
-        console.log('ServiceWorker registration failed: ', err);
-      }
-    });
-  }
   render(App);
 });
 
