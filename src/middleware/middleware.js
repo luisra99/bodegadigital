eaa11e200fda6653073ea7238080a21a7b53b51a
luisra99 <luisraul.alfonsoc@gmail.com>
2023-06-09 21:02:30 +0100

    callback

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -2,8 +2,6 @@ import { Buffer } from 'buffer';
 import express from 'express';
 import https from 'https';
 
-import { getToken } from '@/services/wso2';
-
 import axios from 'axios';
 
 const app = express();
@@ -15,21 +13,17 @@ const instance = axios.create({
   httpsAgent: agent,
 });
 
-const customer_key = import.meta.env.VITE_AM_COSTOMER_KEY;
-const customer_secret = import.meta.env.VITE_AM_COSTOMER_SECRET;
-const protocol = import.meta.env.VITE_AM_PROTOCOL;
-const ip = import.meta.env.VITE_AM_IP;
-const port = import.meta.env.VITE_AM_PORT;
-const token_endpoint = import.meta.env.VITE_AM_TOKEN_ENDPOINT;
-
 app.get('/token', async (req, res) => {
-  const dir = `${ip}${port ? `:${port}` : ''}/oauth2/token`;
-  const dir_token = dir;
-  const basic = Buffer.from(`${customer_key}:${customer_secret}`).toString('base64');
   try {
+    const ip = import.meta.env.VITE_AM_IP;
+    const port = import.meta.env.VITE_AM_PORT;
+    const token_endpoint = `${ip}${port ? `:${port}` : ''}/oauth2/token`;
+    const customer_key = import.meta.env.VITE_AM_COSTUMER_KEY;
+    const customer_secret = import.meta.env.VITE_AM_COSTUMER_SECRET;
+    const basic = Buffer.from(`${customer_key}:${customer_secret}`).toString('base64');
     const response = await instance({
       method: 'post',
-      url: dir_token,
+      url: token_endpoint,
       headers: {
         Authorization: `Basic ${basic}`,
         'Access-Control-Allow-Origin': '*',
@@ -39,9 +33,6 @@ app.get('/token', async (req, res) => {
       },
     });
     const { access_token } = response.data || {};
-    console.log(basic);
-
-    console.log(access_token);
 
     res.json({
       Authorization: `Bearer ${access_token}`,
@@ -51,57 +42,6 @@ app.get('/token', async (req, res) => {
     console.error(`Error occurred while fetching token. Details: ${error.message}`);
     return {};
   }
-  // try {
-  //   axios
-  //     .post('https://identity.enzona.net/oauth2/token')
-  //     .then((response) => console.log(response));
-  // } catch (err) {
-  //   console.log(err);
-  // }
   //TODO: llamar al api de enzona
 });
-// app.get('/eztoken', async (req, res) => {
-//   try {
-//     const response = await axios({
-//       method: 'post',
-//       url: dir_token,
-//       headers: {
-//         Authorization: `Basic ${basic}`,
-//         'Access-Control-Allow-Origin': '*',
-//       },
-//       params: {
-//         grant_type: 'client_credentials',
-//       },
-//     });
-//     const { access_token } = response.data || {};
-//     console.log(basic);
-
-//     console.log(access_token);
-
-//     res.json({
-//       Authorization: `Bearer ${access_token}`,
-//       'Access-Control-Allow-Origin': '*',
-//     });
-//   } catch (error) {
-//     console.error(`Error occurred while fetching token. Details: ${error.message}`);
-//     return {};
-//   }
-//   // try {
-//   //   axios
-//   //     .post('https://identity.enzona.net/oauth2/token')
-//   //     .then((response) => console.log(response));
-//   // } catch (err) {
-//   //   console.log(err);
-//   // }
-//   //TODO: llamar al api de enzona
-// });
-
-// app.use((req, res, next) => {
-//   if (req.path === '/oauth2/token') {
-//     console.log('cors');
-//     req.set('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//   }
-//   next();
-// });
 export const handler = app;
