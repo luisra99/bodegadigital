89600b18c7fff916bf4962287816c7ebac767e49
luisra99 <luisraul.alfonsoc@gmail.com>
2023-06-11 04:37:22 +0100

    hn

--- public/sw.js
+++ public/sw.js
@@ -55,3 +55,30 @@ registerRoute(
     cacheName: CACHE_NAME,
   }),
 );
+//TODO: Investigar
+
+self.addEventListener('periodicsync', (event) => {
+  if (event.tag === 'myPeriodicSync') {
+    console.log('Sync event received');
+    event.waitUntil(doBackgroundSync());
+  }
+});
+
+// Schedule the periodic sync task
+self.registration.periodicSync.register('myPeriodicSync', {
+  minInterval: 24 * 60 * 60 * 1000, // Sync once per day
+});
+//TODO: Investigar
+function doBackgroundSync() {
+  // ... Perform background synchronization logic here ...
+}
+
+self.addEventListener('sync', (event) => {
+  if (event.tag === 'mySync') {
+    event.waitUntil(doSync());
+  }
+});
+
+function doSync() {
+  // ... Perform background synchronization logic here ...
+}
